da777d1fb8a8af7b8dde52209a0577aca76a3a73
yash203 <yashpal.kumar@gmail.com>
2024-09-18 23:58:46 +0530

    day 1 file

--- basics.py
+++ basics.py
@@ -1,1 +1,33 @@
 print("let the lessons start!!")
+intx = 10;
+print('value of intx is '+(str(intx)))
+
+intx = 'stringVal'
+
+print('value of intx after dynamic typing is now ', intx);
+
+#input always returns a string
+#intx = input("pls enter any value you want to assign to intx : ");
+print('intx now  ', intx);
+print("hello\example\\test.txt");
+A=16
+B=15
+print(A%B//A)
+
+
+#range(stop) - from 0 till one minus stop 
+#rante(start, stop) - from start till one minus stop
+#range(start, stop, step) - from start till one minus stop with hops in steps, step if -ve then reduces
+# start, stop, step all must be integers
+#for p in range(10, 3, -2):
+ #   print(p)
+
+print('mina\neina\ndika')
+s="WELCOME"
+print(s[0::2])
+
+#opening a file in read mode
+myfile = open("sample.txt","r")
+fileContents = myfile.read()
+print(fileContents)
+myfile.close();
